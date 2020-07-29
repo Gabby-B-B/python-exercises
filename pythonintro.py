0a60bc676b470f81be5b2bc9c2819157bf8941b8
Gabby Broussard <gabriella.b.broussard@gmail.com>
2020-07-29 11:59:02 -0500

    jupyter practice

---
+++ pythonintro.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+greeting= "Good Morning Darden!"
+print(greeting)
+greeting.lower()
+
+
+# In[3]:
+
+
+greeting.upper()
+
+
+# In[4]:
+
+
+greets= "Have a great day of learning!"
+print(greeting, greets)
+
+
+# In[ ]:
+
+
+
+
